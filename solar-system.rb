769bfd2659ca4b57017ee18fbec59de990709a0b
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-02-08 15:54:54 -0800

    Add Plannets

--- solar-system.rb
+++ solar-system.rb
@@ -6,6 +6,11 @@ def initialize(planets)
   @planets = planets
 end
 
+def add(new_planet)
+  @planets.push(new_planet)
+
+end
+
 def summary
   return @planets
 end
@@ -15,4 +20,5 @@ end
 
 planet_array =["Mercury", "Venus", "Earth"]
 our_solar_system = Solar_System.new(planet_array)
+our_solar_system.add("Mars")
 puts our_solar_system.summary

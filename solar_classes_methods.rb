fc9c2c0e70e260fa9ac46a148d512b9a0179ee32
torshimizu <torshimizu@gmail.com>
2018-02-11 23:04:30 -0800

    Update solar_classes_methods.rb

--- solar_classes_methods.rb
+++ solar_classes_methods.rb
@@ -1,13 +1,13 @@
 class Planet
   attr_reader :name
-  attr_accessor :year_length, :distance_from_sun
+  attr_accessor :year_length, :distance_from_sun, :diameter
 
   def initialize(planet_name)
     @name = planet_name
   end
 
   def planet_summary
-    return "#{name}: \n Takes #{year_length} to orbit the sun. \n It's distance from the sun is #{distance_from_sun} miles."
+    return "#{name}: \n Takes #{year_length} Earth years to orbit the sun. \n It's distance from the sun is #{distance_from_sun} km. \n #{name}\'s diameter is #{diameter} km."
   end
 end
 
@@ -21,6 +21,7 @@ class SolarSystem
     @all_summary = Array.new
     @planet_names = Array.new
     @planet_hash = Hash.new
+    create_all_summary
     create_planet_hash
     planet_names
 
@@ -49,12 +50,8 @@ class SolarSystem
     end
   end
 
-  # def list_planet_names
-  #   @planet_names
-  # end
-
   def listplanets
-    numbered_planets = []
+    numbered_planets = ["0: List all summaries"]
     @planets.each.with_index(1) do |planet, index|
       numbered_planet = "#{index}: #{planet.name}"
       numbered_planets << numbered_planet
@@ -63,7 +60,7 @@ class SolarSystem
     return numbered_planets
   end
 
-  # ______ these methods are not used in UI ______
+# ________ the following methods can be produced by looping over @planets and printing each planet's summary
   def create_all_summary
     @planets.each { |planet| @all_summary << planet.planet_summary }
   end
@@ -75,10 +72,51 @@ class SolarSystem
 end
 
 
-# ________ methods when adding a new planet _________
+# ________ methods for other options _________
 
-def get_solar_system(menu)
-  puts "Which solar system is the planet in? "
+
+def get_planet_name
+  print "What is the name your new planet? > "
+  new_planet_name = gets.chomp.capitalize
+  return new_planet_name
+end
+
+def get_distance # might need to convert ',' to '_' for bignums
+  print "How far is this planet from the Sun in miles? > "
+  miles_from_sun = gets.chomp
+  miles_from_sun = match_number(miles_from_sun)
+end
+
+def get_year
+  print "How many Earth years does it take to orbit the Sun? > "
+  planet_year = gets.chomp
+  miles_from_sun = match_number(planet_year)
+end
+
+def match_number(num)
+  until num.match(/^\-?\d*\.?\d*$/) && !num.empty?
+    if !num.match(/^\-?\d*\.?\d*$/)
+      print "Invalid input. Please enter a number without commas: "
+      num = gets.chomp
+    end
+    if num.empty?
+      print "No number inputted. Please enter a number: "
+      num = gets.chomp
+    end
+  end
+  return num
+end
+
+# _________ methods for user interface _________
+
+def print_menu(menu)
+  menu.each.with_index(1) do |sol_sys, listno|
+    puts "#{listno}: #{sol_sys.sys_name}"
+  end
+  puts
+end
+
+def get_solar_system(menu) # not sure why I have both this method and choose_system
   print_menu(menu)
   print "> "
   users_system = gets.chomp.to_i
@@ -90,31 +128,6 @@ def get_solar_system(menu)
     puts "WARNING: I don't know that system."
   end
   return users_system
-end
-
-def get_planet_name
-  print "What is the name your new planet? > "
-  new_planet_name = gets.chomp.capitalize
-  return new_planet_name
-end
-
-def get_distance # might need to convert ',' to '_' for bignums
-  print "How far is this planet from the Sun in miles? > "
-  miles_from_sun = gets.chomp.to_i
-end
-
-def get_year
-  print "How many Earth years does it take to orbit the Sun? > "
-  planet_year = gets.chomp.to_f
-end
-
-# _________ methods for user interface _________
-
-def print_menu(menu)
-  menu.each.with_index(1) do |sol_sys, listno|
-    puts "#{listno}: #{sol_sys.sys_name}"
-  end
-  puts
 end
 
 def choose_system(menu, chosen_system) # I'm not sure why I made this a method
@@ -134,7 +147,7 @@ def choose_system(menu, chosen_system) # I'm not sure why I made this a method
     case loop_menu
     when "yes", "y"
       print_menu(menu)
-      working_system = choose_system(menu) # this is called recursion(?)
+      working_system = choose_system(menu, chosen_system) # this is called recursion(?)
     when "no", "n"
       loop_menu = false
       puts
@@ -147,7 +160,11 @@ end
 def choose_planet(working_system)
   puts working_system.listplanets
   puts
-  print "Please select the number of the planet you want to see: "
+  print "Please select the number of the item you would like to see: "
+end
+
+def print_other_options(other_options)
+  other_options.each { |num, option| puts "#{num}: #{option}" }
 end
 
 def keep_looping(input_variable)
